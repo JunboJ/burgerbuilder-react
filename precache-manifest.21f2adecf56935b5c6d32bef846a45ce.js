ae2c935a02300da2ab09525b7af73d616fafcb36
JunboJ <junboz598@gmail.com>
2019-11-14 13:24:02 +1300

    Updates

---
+++ precache-manifest.21f2adecf56935b5c6d32bef846a45ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bce16b82433912aec401f2707adcc0",
+    "url": "/burger-react/index.html"
+  },
+  {
+    "revision": "db7f3211f9f6e537eaac",
+    "url": "/burger-react/static/css/main.29158acc.chunk.css"
+  },
+  {
+    "revision": "b61ca20a24cf35ab9c0a",
+    "url": "/burger-react/static/js/2.7688aca9.chunk.js"
+  },
+  {
+    "revision": "db7f3211f9f6e537eaac",
+    "url": "/burger-react/static/js/main.9d1a21c8.chunk.js"
+  },
+  {
+    "revision": "af0d5b71efb4581c2de1",
+    "url": "/burger-react/static/js/runtime-main.229d3afc.js"
+  },
+  {
+    "revision": "5335e272056e3dc1914c4a67985ab22a",
+    "url": "/burger-react/static/media/logo.5335e272.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.781f7c93a8cbfcfa918456b253b460c5.js"
+  "/burger-react/precache-manifest.21f2adecf56935b5c6d32bef846a45ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
