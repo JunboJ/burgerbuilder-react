0df03def4ac9626f5b101629c730bc7c7298922a
JunboJ <junboz598@gmail.com>
2020-08-23 22:27:58 +1200

    Updates

---
+++ precache-manifest.e3600e4dc5d1ad40e2b71966ac866ef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3380b05291580ed413098c9c42298e",
+    "url": "/burgerbuilder-react/index.html"
+  },
+  {
+    "revision": "6882c90863dfc04c4612",
+    "url": "/burgerbuilder-react/static/css/main.5316e9a2.chunk.css"
+  },
+  {
+    "revision": "c2a5e7496f9abedaa18d",
+    "url": "/burgerbuilder-react/static/js/2.32bb834f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/burgerbuilder-react/static/js/2.32bb834f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6882c90863dfc04c4612",
+    "url": "/burgerbuilder-react/static/js/main.74c4f90e.chunk.js"
+  },
+  {
+    "revision": "8eb541065ad44010be19",
+    "url": "/burgerbuilder-react/static/js/runtime-main.c7522d2f.js"
+  },
+  {
+    "revision": "5335e272056e3dc1914c4a67985ab22a",
+    "url": "/burgerbuilder-react/static/media/logo.5335e272.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burgerbuilder-react/precache-manifest.26325ece0d477537f85b13589df5afcf.js"
+  "/burgerbuilder-react/precache-manifest.e3600e4dc5d1ad40e2b71966ac866ef0.js"
 );
 
 self.addEventListener('message', (event) => {
